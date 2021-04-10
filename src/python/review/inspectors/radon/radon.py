a442f84ab95ab971a9020d38f43eebea8c1a8076
Ilya Vlasov <ilyavlasov2011@gmail.com>
2021-04-10 13:53:07 +0300

    Small code refactoring:
    1) Replaced __get_maintainability_lack with convert_percentage_of_value_to_lack_of_value;
    2) Added some comments.

--- src/python/review/inspectors/radon/radon.py
+++ src/python/review/inspectors/radon/radon.py
@@ -7,6 +7,7 @@ from src.python.review.common.subprocess_runner import run_in_subprocess
 from src.python.review.inspectors.base_inspector import BaseInspector
 from src.python.review.inspectors.inspector_type import InspectorType
 from src.python.review.inspectors.issue import BaseIssue, IssueData, IssueType, MaintainabilityLackIssue
+from src.python.review.inspectors.common import convert_percentage_of_value_to_lack_of_value
 
 
 class RadonInspector(BaseInspector):
@@ -15,9 +16,9 @@ class RadonInspector(BaseInspector):
     @classmethod
     def inspect(cls, path: Path, config: dict) -> List[BaseIssue]:
         mi_command = [
-            "radon", "mi",
-            "--max", "F",
-            "--show",
+            "radon", "mi",  # compute the Maintainability Index score
+            "--max", "F",  # set the maximum MI rank to display
+            "--show",  # actual MI value is shown in results, alongside the rank
             path,
         ]
 
@@ -28,6 +29,7 @@ class RadonInspector(BaseInspector):
     def mi_parse(cls, mi_output: str) -> List[BaseIssue]:
         """
         Parses the results of the "mi" command.
+        Description: https://radon.readthedocs.io/en/latest/commandline.html#the-mi-command
 
         :param mi_output: "mi" command output.
         :return: list of issues.
@@ -37,7 +39,7 @@ class RadonInspector(BaseInspector):
         issues: List[BaseIssue] = []
         for groups in row_re.findall(mi_output):
             file_path = Path(groups[0])
-            maintainability_lack = cls.__get_maintainability_lack(float(groups[1]))
+            maintainability_lack = convert_percentage_of_value_to_lack_of_value(float(groups[1]))
 
             issue_data = IssueData.get_base_issue_data_dict(file_path, cls.inspector_type)
             issue_data[IssueData.DESCRIPTION.value] = ""  # TODO: add tip
@@ -47,14 +49,3 @@ class RadonInspector(BaseInspector):
             issues.append(MaintainabilityLackIssue(**issue_data))
 
         return issues
-
-    @staticmethod
-    def __get_maintainability_lack(maintainability_index: float) -> int:
-        """
-        Converts maintainability index to lack of maintainability.
-        Calculated by the formula: floor(100 - maintainability_index).
-
-        :param maintainability_index: value in the range from 0 to 100.
-        :return: lack of maintainability.
-        """
-        return floor(100 - maintainability_index)
