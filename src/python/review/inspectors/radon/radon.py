62600e33e163c196fa91b1682768a2d15e7e4c01
Ilya Vlasov <ilyavlasov2011@gmail.com>
2021-04-10 17:04:15 +0300

    Added maintainability index tip

--- src/python/review/inspectors/radon/radon.py
+++ src/python/review/inspectors/radon/radon.py
@@ -7,6 +7,7 @@ from src.python.review.inspectors.base_inspector import BaseInspector
 from src.python.review.inspectors.inspector_type import InspectorType
 from src.python.review.inspectors.issue import BaseIssue, IssueData, IssueType, MaintainabilityLackIssue
 from src.python.review.inspectors.common import convert_percentage_of_value_to_lack_of_value
+from src.python.review.inspectors.tips import get_maintainability_index_tip
 
 
 class RadonInspector(BaseInspector):
@@ -41,7 +42,7 @@ class RadonInspector(BaseInspector):
             maintainability_lack = convert_percentage_of_value_to_lack_of_value(float(groups[1]))
 
             issue_data = IssueData.get_base_issue_data_dict(file_path, cls.inspector_type)
-            issue_data[IssueData.DESCRIPTION.value] = ""  # TODO: add tip
+            issue_data[IssueData.DESCRIPTION.value] = get_maintainability_index_tip()
             issue_data[IssueData.MAINTAINABILITY_LACK.value] = maintainability_lack
             issue_data[IssueData.ISSUE_TYPE.value] = IssueType.MAINTAINABILITY
 
