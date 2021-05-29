3b82f8d23acf18375a440dfcc0fc78bcf2868e84
diatlova <dari.diatlova@gmail.com>
2021-05-29 16:39:50 +0300

    improved dataset class

---
+++ src/python/model/dataset/dataset.py
@@ -0,0 +1,31 @@
+import logging
+
+import pandas as pd
+import torch
+from src.python.model.common.util import MarkingArgument
+from transformers import RobertaTokenizer
+
+logger = logging.getLogger(__name__)
+
+
+class QodanaDataset(torch.utils.data.Dataset):
+    """ MarkingArgument.ID.value is a an id of the solution that corresponds to the line
+        MarkingArgument.INSPECTIONS.value is a is a target column name in dataset
+        ColumnName.CODE.value is an observation column name in dataset where lines of code are stored
+    """
+
+    def __init__(self, data_path: str, context_length: int):
+        super().__init__()
+        df = pd.read_csv(data_path)
+        tokenizer = RobertaTokenizer.from_pretrained('roberta-base')
+        code = list(map(str, df['code']))
+        self.target = torch.tensor(df.iloc[:, 2:].values)
+        self.code_encoded = tokenizer(
+            code, padding=True, truncation=True, max_length=context_length, return_tensors="pt"
+        )[MarkingArgument.INPUT_IDS.value]
+
+    def __getitem__(self, idx):
+        return self.code_encoded[idx], self.target[idx]
+
+    def __len__(self):
+        return len(self.target)
