057031f52e4c73a531ed1110a6db44f79cc9bf80
Ilya Vlasov <ilyavlasov2011@gmail.com>
2021-05-30 17:41:08 +0300

    Fix double quotes

--- src/python/evaluation/common/util.py
+++ src/python/evaluation/common/util.py
@@ -19,16 +19,16 @@ class EvaluationArgument(Enum):
     RESULT_FILE_NAME_EXT = f'{RESULT_FILE_NAME}{Extension.XLSX.value}'
 
 
-script_structure_rule = ("Please, make sure your XLSX-file matches following script standards: \n"
-                         "1. Your XLSX-file should have 2 obligatory columns named:"
-                         f"'{ColumnName.CODE.value}' & '{ColumnName.LANG.value}'. \n"
-                         f"'{ColumnName.CODE.value}' column -- relates to the code-sample. \n"
-                         f"'{ColumnName.LANG.value}' column -- relates to the language of a "
-                         "particular code-sample. \n"
-                         "2. Your code samples should belong to the one of the supported languages. \n"
-                         "Supported languages are: Java, Kotlin, Python. \n"
-                         f"3. Check that '{ColumnName.LANG.value}' column cells are filled with "
-                         "acceptable language-names: \n"
-                         f"Acceptable language-names are: {LanguageVersion.PYTHON_3.value}, "
-                         f"{LanguageVersion.JAVA_8.value} ,"
-                         f"{LanguageVersion.JAVA_11.value} and {LanguageVersion.KOTLIN.value}.")
+script_structure_rule = ('Please, make sure your XLSX-file matches following script standards: \n'
+                         '1. Your XLSX-file should have 2 obligatory columns named:'
+                         f'"{ColumnName.CODE.value}" & "{ColumnName.LANG.value}". \n'
+                         f'"{ColumnName.CODE.value}" column -- relates to the code-sample. \n'
+                         f'"{ColumnName.LANG.value}" column -- relates to the language of a '
+                         'particular code-sample. \n'
+                         '2. Your code samples should belong to the one of the supported languages. \n'
+                         'Supported languages are: Java, Kotlin, Python. \n'
+                         f'3. Check that "{ColumnName.LANG.value}" column cells are filled with '
+                         'acceptable language-names: \n'
+                         f'Acceptable language-names are: {LanguageVersion.PYTHON_3.value}, '
+                         f'{LanguageVersion.JAVA_8.value} ,'
+                         f'{LanguageVersion.JAVA_11.value} and {LanguageVersion.KOTLIN.value}.')
