4b379ba6d5fbc70a32dc63d4bfec2283c729463b
Daria Diatlova <dari.diatlova@gmail.com>
2021-06-01 02:48:07 +0300

    fixed label choice while computing metric

--- src/python/model/common/metric.py
+++ src/python/model/common/metric.py
@@ -3,11 +3,11 @@ from sklearn.metrics import multilabel_confusion_matrix
 
 
 class Metric:
-    def __init__(self, threshold: float):
+    def __init__(self, threshold: float, num_labels: int):
         self.threshold = threshold
 
-    def get_f1_score(self, predictions: torch.tensor, labels: torch.tensor) -> float:
-        confusion_matrix = multilabel_confusion_matrix(labels, predictions)
+    def get_f1_score(self, predictions: torch.tensor, targets: torch.tensor) -> float:
+        confusion_matrix = multilabel_confusion_matrix(targets, predictions, labels=num_labels)
         false_positives = sum([score[0][1] for score in confusion_matrix])
         false_negatives = sum([score[1][0] for score in confusion_matrix])
         true_positives = sum([score[1][1] for score in confusion_matrix])
@@ -15,7 +15,7 @@ class Metric:
         return f1_score
 
     def compute_metric(self, evaluation_predictions: torch.tensor) -> dict:
-        logits, labels = evaluation_predictions
+        logits, targets = evaluation_predictions
         prediction_probabilities = torch.from_numpy(logits).sigmoid()
         predictions = torch.where(prediction_probabilities > self.threshold, 1, 0)
-        return {'f1_score': self.get_f1_score(predictions, torch.tensor(labels))}
+        return {'f1_score': self.get_f1_score(predictions, torch.tensor(targets))}
