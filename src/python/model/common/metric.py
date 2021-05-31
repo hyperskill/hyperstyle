30188d4601e3e0858962645d2303ae07f7e542a2
Daria Diatlova <dari.diatlova@gmail.com>
2021-06-01 02:54:13 +0300

    updated metric

--- src/python/model/common/metric.py
+++ src/python/model/common/metric.py
@@ -5,9 +5,10 @@ from sklearn.metrics import multilabel_confusion_matrix
 class Metric:
     def __init__(self, threshold: float, num_labels: int):
         self.threshold = threshold
+        self.num_labels = num_labels
 
     def get_f1_score(self, predictions: torch.tensor, targets: torch.tensor) -> float:
-        confusion_matrix = multilabel_confusion_matrix(targets, predictions, labels=range(num_labels))
+        confusion_matrix = multilabel_confusion_matrix(targets, predictions, labels=range(self.num_labels))
         false_positives = sum([score[0][1] for score in confusion_matrix])
         false_negatives = sum([score[1][0] for score in confusion_matrix])
         true_positives = sum([score[1][1] for score in confusion_matrix])
