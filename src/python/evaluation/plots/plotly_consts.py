005975bbb777fcd8170e09b0582af4d2520d7a50
Ilya Vlasov <ilyavlasov2011@gmail.com>
2021-06-04 16:19:47 +0300

    Added plotly_consts.py

---
+++ src/python/evaluation/plots/plotly_consts.py
@@ -0,0 +1,13 @@
+from enum import Enum
+
+
+class MARGIN(Enum):
+    ZERO = {'l': 0, 'r': 0, 'b': 0, 't': 0}
+    DEFAULT = {}
+
+
+class SORT_ORDER(Enum):  # noqa: N801
+    CATEGORY_ASCENDING = 'category ascending'
+    CATEGORY_DESCENDING = 'category descending'
+    TOTAL_ASCENDING = 'total ascending'
+    TOTAL_DESCENDING = 'total descending'
