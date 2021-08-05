012456bcddfaec771e31280cf6488d5e51956368
Ilya Vlasov <ilyavlasov2011@gmail.com>
2021-08-05 13:51:57 +0300

    Added a new test and refactored the old one

--- test/python/functional_tests/test_json_format.py
+++ test/python/functional_tests/test_json_format.py
@@ -5,6 +5,25 @@ from test.python.functional_tests.conftest import DATA_PATH, LocalCommandBuilder
 import pytest
 from jsonschema import validate
 
+
+def _get_output_json(local_command: LocalCommandBuilder, new_format: bool) -> str:
+    project_path = DATA_PATH / 'file_or_project' / 'project'
+
+    local_command.verbosity = 0
+    local_command.format = 'json'
+    local_command.path = project_path
+    local_command.new_format = new_format
+
+    process = subprocess.run(
+        local_command.build(),
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+    )
+    stdout = process.stdout.decode()
+
+    return json.loads(stdout)
+
+
 OLD_FORMAT_SCHEMA = {
     'type': 'object',
     'additionalProperties': False,
@@ -88,27 +107,94 @@ NEW_FORMAT_SCHEMA = {
     },
 }
 
-JSON_FORMAT_TEST_DATA = [
+JSON_FORMAT_SCHEMA_TEST_DATA = [
     (False, OLD_FORMAT_SCHEMA),
     (True, NEW_FORMAT_SCHEMA),
 ]
 
 
-@pytest.mark.parametrize(('new_format', 'schema'), JSON_FORMAT_TEST_DATA)
-def test_json_format(local_command: LocalCommandBuilder, new_format: bool, schema: str):
-    project_path = DATA_PATH / 'file_or_project' / 'project'
+@pytest.mark.parametrize(('new_format', 'schema'), JSON_FORMAT_SCHEMA_TEST_DATA)
+def test_json_format_schema(local_command: LocalCommandBuilder, new_format: bool, schema: str):
+    validate(_get_output_json(local_command, new_format), schema)
 
-    local_command.verbosity = 0
-    local_command.format = 'json'
-    local_command.path = project_path
-    local_command.new_format = new_format
 
-    process = subprocess.run(
-        local_command.build(),
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE,
-    )
-    stdout = process.stdout.decode()
+ISSUES = [
+    {
+        'code': 'W0612',
+        'text': 'Unused variable \'a\'',
+        'line': 'a = 1',
+        'line_number': 2,
+        'column_number': 5,
+        'category': 'BEST_PRACTICES',
+        'influence_on_penalty': 0,
+    },
+    {
+        'code': 'W0612',
+        'text': 'Unused variable \'b\'',
+        'line': 'b = 2',
+        'line_number': 3,
+        'column_number': 5,
+        'category': 'BEST_PRACTICES',
+        'influence_on_penalty': 0,
+    },
+    {
+        'code': 'W0612',
+        'text': 'Unused variable \'c\'',
+        'line': 'c = 3',
+        'line_number': 4,
+        'column_number': 5,
+        'category': 'BEST_PRACTICES',
+        'influence_on_penalty': 0,
+    },
+]
 
-    output_json = json.loads(stdout)
-    validate(output_json, schema)
+OLD_FORMAT_EXPECTED_JSON = {
+    'quality': {
+        'code': 'EXCELLENT',
+        'text': 'Code quality (beta): EXCELLENT',
+    },
+    'issues': ISSUES,
+}
+
+NEW_FORMAT_EXPECTED_JSON = {
+    'quality': {
+        'code': 'EXCELLENT',
+        'text': 'Code quality (beta): EXCELLENT',
+    },
+    'file_review_results': [
+        {
+            'file_name': '__init__.py',
+            'quality': {
+                'code': 'EXCELLENT',
+                'text': 'Code quality (beta): EXCELLENT',
+            },
+            'issues': [],
+        },
+        {
+            'file_name': 'one.py',
+            'quality': {
+                'code': 'EXCELLENT',
+                'text': 'Code quality (beta): EXCELLENT',
+            },
+            'issues': [],
+        },
+        {
+            'file_name': 'other.py',
+            'quality': {
+                'code': 'GOOD',
+                'text': 'Code quality (beta): GOOD',
+            },
+            'issues': ISSUES,
+        },
+    ],
+}
+
+JSON_FORMAT_TEST_DATA = [
+    (False, OLD_FORMAT_EXPECTED_JSON),
+    (True, NEW_FORMAT_EXPECTED_JSON),
+]
+
+
+@pytest.mark.parametrize(('new_format', 'expected_json'), JSON_FORMAT_TEST_DATA)
+def test_json_format(local_command: LocalCommandBuilder, new_format: bool, expected_json: str):
+    assert _get_output_json(local_command, new_format) == expected_json
