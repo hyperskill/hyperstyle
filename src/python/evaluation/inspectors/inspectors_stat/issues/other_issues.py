c284a49e36271bae2113dd6362ff72536cb24f7b
Anastasiia.Birillo <nbirillo@mail.ru>
2021-08-23 16:10:22 +0300

    Delete evaluation

--- src/python/evaluation/inspectors/inspectors_stat/issues/other_issues.py
+++
@@ -1,8 +0,0 @@
-PYTHON_RADON_ISSUES = {
-    'RAD100': 'MAINTAINABILITY index',
-}
-
-PYTHON_AST_ISSUES = {
-    'C001': 'Boolean expressions length',
-    'C002': 'Functions length',
-}
--- src/python/evaluation/qodana/imitation_model/__init__.py
+++
@@ -1,3 +0,0 @@
-from src.python import MAIN_FOLDER
-
-MODEL_FOLDER = MAIN_FOLDER.parent / 'python/imitation_model'
