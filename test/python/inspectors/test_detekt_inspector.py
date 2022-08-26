52fdda7145b5180cb3e8e72aa4c9254294908a29
GirZ0n <ilyavlasov2011@gmail.com>
2022-08-26 08:40:14 +0300

    Add new tests for pylint

--- test/python/inspectors/test_detekt_inspector.py
+++ test/python/inspectors/test_detekt_inspector.py
@@ -1,8 +1,13 @@
-from test.python.inspectors import KOTLIN_DATA_FOLDER
+from test.python.inspectors import DETEKT_DATA_FOLDER, KOTLIN_DATA_FOLDER
 from test.python.inspectors.conftest import use_file_metadata
 
 import pytest
 from hyperstyle.src.python.review.common.language import Language
+from hyperstyle.src.python.review.inspectors.common.tips import (
+    get_bool_expr_len_tip,
+    get_cyclomatic_complexity_tip,
+    get_func_len_tip, get_magic_number_tip,
+)
 from hyperstyle.src.python.review.inspectors.detekt.detekt import DetektInspector
 from hyperstyle.src.python.review.reviewers.utils.issues_filter import filter_low_measure_issues
 
@@ -42,3 +47,44 @@ def test_file_with_issues(file_name: str, n_issues: int):
         issues = filter_low_measure_issues(issues, Language.KOTLIN)
 
     assert len(issues) == n_issues
+
+
+MEASURE_TEST_DATA = [
+    ('LongMethod', 15),
+    ('ComplexCondition', 4),
+    ('ComplexMethod', 17),
+]
+
+
+@pytest.mark.parametrize(('origin_class', 'expected_measure'), MEASURE_TEST_DATA)
+def test_measure_parse(origin_class: str, expected_measure: int):
+    inspector = DetektInspector()
+
+    path_to_file = DETEKT_DATA_FOLDER / 'issues' / f'{origin_class}.kt'
+    with use_file_metadata(path_to_file) as file_metadata:
+        issues = inspector.inspect(file_metadata.path, {})
+
+    issue = list(filter(lambda elem: elem.origin_class == origin_class, issues))[0]
+
+    assert issue.measure() == expected_measure
+
+
+NEW_DESCRIPTION_TEST_DATA = [
+    ('LongMethod', get_func_len_tip().format(15)),
+    ('ComplexCondition', get_bool_expr_len_tip(unit_name='operands').format(4)),
+    ('ComplexMethod', get_cyclomatic_complexity_tip().format(17)),
+    ('MagicNumber', get_magic_number_tip(with_number_field=False)),
+]
+
+
+@pytest.mark.parametrize(('origin_class', 'expected_description'), NEW_DESCRIPTION_TEST_DATA)
+def test_new_issue_description(origin_class: str, expected_description: str):
+    inspector = DetektInspector()
+
+    path_to_file = DETEKT_DATA_FOLDER / 'issues' / f'{origin_class}.kt'
+    with use_file_metadata(path_to_file) as file_metadata:
+        issues = inspector.inspect(file_metadata.path, {})
+
+    issue = list(filter(lambda elem: elem.origin_class == origin_class, issues))[0]
+
+    assert issue.description == expected_description
