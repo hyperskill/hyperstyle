18e4856b82a7b03e3625208beed4dc0dcfa28e3c
Anastasiia.Birillo <nbirillo@mail.ru>
2022-09-29 20:39:54 +0200

    Try to fix teamcity

--- hyperstyle/src/python/review/reviewers/common.py
+++ hyperstyle/src/python/review/reviewers/common.py
@@ -73,21 +73,15 @@ def perform_language_review(metadata: Metadata, config: ApplicationConfig, langu
         if not config.allow_duplicates:
             issues = filter_duplicate_issues(issues)
 
+    if isinstance(metadata, FileMetadata):
+        files_metadata = [metadata]
+        issues = filter_out_of_range_issues(issues, config.start_line, config.end_line)
+    else:
+        files_metadata = metadata.language_to_files[language]
+
     file_path_to_issues = defaultdict(list)
     for issue in issues:
         file_path_to_issues[issue.file_path].append(issue)
-
-    if isinstance(metadata, FileMetadata):
-        current_files = [metadata]
-        issues = filter_out_of_range_issues(issues, config.start_line, config.end_line)
-    else:
-        current_files = metadata.language_to_files[language]
-    # else:
-    #     files = file_path_to_issues.keys()
-    #     if len(files) == 0:
-    #         current_files = []
-    #     else:
-    #         current_files = files
 
     previous_issues = get_previous_issues_by_language(config.history, language)
     categorize(previous_issues, issues)
@@ -103,12 +97,12 @@ def perform_language_review(metadata: Metadata, config: ApplicationConfig, langu
     }
 
     file_review_results = []
-    for file in current_files:
-        file_issues = file_path_to_issues[file.path]
+    for file_metadata in files_metadata:
+        file_issues = file_path_to_issues[file_metadata.path]
         file_issues_by_difficulty = group_issues_by_difficulty(file_issues)
 
         code_statistics_by_difficulty = {
-            difficulty: gather_code_statistics(file_issues, file.path)
+            difficulty: gather_code_statistics(file_issues, file_metadata.path)
             for difficulty, file_issues in file_issues_by_difficulty.items()
         }
 
@@ -130,7 +124,7 @@ def perform_language_review(metadata: Metadata, config: ApplicationConfig, langu
             general_quality_by_difficulty[difficulty] = general_quality_by_difficulty[difficulty].merge(quality)
 
         file_review_results.append(
-            FileReviewResult(quality_by_difficulty, punisher_by_difficulty, file_issues, file.path),
+            FileReviewResult(quality_by_difficulty, punisher_by_difficulty, file_issues, file_metadata.path),
         )
 
     return GeneralReviewResult(
