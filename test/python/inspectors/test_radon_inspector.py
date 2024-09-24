d0287cd53bae895992ec0920066de22d94c68960
meanmail <meanmail@meanmail.dev>
2024-09-24 15:41:41 +0200

    Rename 'maintainability_index' to 'maintainability_lack'
    Refactor test assertions to use the updated attribute name 'maintainability_lack' instead of 'maintainability_index' for consistency. This change affects all occurrences within the test case to ensure accuracy in attribute checking.

--- test/python/inspectors/test_radon_inspector.py
+++ test/python/inspectors/test_radon_inspector.py
@@ -51,11 +51,11 @@ def test_mi_parse() -> None:
         IssueType.MAINTAINABILITY,
         IssueType.MAINTAINABILITY,
     ]
-    assert hasattr(issues[0], "maintainability_index")
-    assert issues[0].maintainability_index == 95
+    assert hasattr(issues[0], "maintainability_lack")
+    assert issues[0].maintainability_lack == 95
 
-    assert hasattr(issues[1], "maintainability_index")
-    assert issues[1].maintainability_index == 86
+    assert hasattr(issues[1], "maintainability_lack")
+    assert issues[1].maintainability_lack == 86
 
-    assert hasattr(issues[2], "maintainability_index")
-    assert issues[2].maintainability_index == 30
+    assert hasattr(issues[2], "maintainability_lack")
+    assert issues[2].maintainability_lack == 30
